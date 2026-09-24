ef8bf03a9294ecbded119b259ed6f3026f6a9a4b
Chad Kennedy <57039655+chadakenn@users.noreply.github.com>
2026-09-24 18:08:29 -0400

    Add branded desktop startup screen

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,4 +3,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("beamloomDesktop", {
   displays: () => ipcRenderer.invoke("beamloom:displays"),
   openProjector: (displayId) => ipcRenderer.invoke("beamloom:open-projector", displayId ?? null),
+  editorReady: () => ipcRenderer.send("beamloom:editor-ready"),
 });
